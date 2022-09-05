90430bf03aea261aaa71b65d4515c5e2e81c104a
Ashis Pavan <ashispavan@live.com>
2022-09-05 12:01:23 -0400

    Initial Commit

---
+++ DontDo/DontDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DontDoApp.swift
+//  DontDo
+//
+//  Created by admin on 2022-09-05.
+//
+
+import SwiftUI
+
+@main
+struct DontDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
